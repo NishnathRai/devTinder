cf48bc7257b3e5c92efa727a1d9383877492bede
Nishnath Rai <nishnathnishu1122@gmail.com>
2024-11-15 16:28:15 +0530

    dynamic signup

--- src/app.js
+++ src/app.js
@@ -1,9 +1,25 @@
 const express = require("express");
 const app = express();
 const PORT = 3000;
-app.listen(PORT,()=>{
-    console.log("server started at port "+PORT);
+const UserModel = require("./models/usermodel.js");
+////Connect to data base
+const {connect} = require("./config/database.js");
+/////create a server
+connect()
+.then(()=>{
+    console.log("connected to database");
+    app.listen(PORT,()=>{
+        console.log("server started at port "+PORT);
+    });
+})
+.catch((err)=>{
+    console.log(err);
 });
-app.use("/",(req,res)=>{
-    res.send("Hellddddo world");
+///////
+app.use(express.json());
+app.post("/signup",async (req,res)=>{
+    const userObj = req.body ;
+    const user = new UserModel(userObj);
+    await user.save();
+    res.send("added user");
 });
